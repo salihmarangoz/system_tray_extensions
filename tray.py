b31a8d339981034b41371f379014a9082f9b2314
salihmarangoz <salih285@gmail.com>
2021-08-08 01:53:57 +0200

    added more options

--- tray.py
+++ tray.py
@@ -17,17 +17,62 @@ tray = QSystemTrayIcon()
 tray.setIcon(icon)
 tray.setVisible(True)
 
+menu = QMenu()
+
+turnoff = QAction("Turn Off")
+menu.addAction(turnoff)
+exec("turnoff.triggered.connect(lambda: ite.turn_off() )")
+
+testpattern = QAction("Test Pattern")
+menu.addAction(testpattern)
+exec("testpattern.triggered.connect(lambda: ite.test_pattern() )")
+
+freeze = QAction("Freeze")
+menu.addAction(freeze)
+exec("freeze.triggered.connect(lambda: ite.freeze() )")
+
+
+menu.addSeparator()
+
+incbrightness = QAction("Increase Brightness")
+menu.addAction(incbrightness)
+exec("incbrightness.triggered.connect(lambda: ite.set_brightness(ite.get_brightness()+10) )")
+
+decbrightness = QAction("Decrease Brightness")
+menu.addAction(decbrightness)
+exec("decbrightness.triggered.connect(lambda: ite.set_brightness(ite.get_brightness()-10) )")
+
+menu.addSeparator()
+
+
+colors = {"red":    (255,   0,   0),
+          "orange": (255,  28,   0),
+          "yellow": (255, 119,   0),
+          "green":  (  0, 255,   0),
+          "blue":   (  0,   0, 255),
+          "teal":   (  0, 255, 255),
+          "purple": (255,   0, 255),
+          }
+
+qactions_colors = {}
+for k,v in colors.items():
+    action = QAction(k.capitalize())
+    qactions_colors[k] = action
+    menu.addAction(action)
+    exec("action.triggered.connect(lambda: ite.set_color("+str(v)+"))")
+
+menu.addSeparator()
 
 # Creating the options dynamically
-menu = QMenu()
-qactions = {}
-qactions_f = {}
+qactions_effects = {}
 for k,v in ite8291r3.effects.items():
     action = QAction(k.capitalize())
-    qactions[k] = action
+    qactions_effects[k] = action
     menu.addAction(action)
     exec("action.triggered.connect(lambda: ite.set_effect( ite8291r3.effects[\""+k+"\"]()) )")
 
+
+menu.addSeparator()
 
 # To quit the app
 quit = QAction("Quit")
