86013fd1460be1b7ef7095c3b53460e7bd4df0aa
salihmarangoz <salih285@gmail.com>
2021-10-04 04:04:20 +0200

    updated test

--- modules/RgbKeyboard/mss_test.py
+++ modules/RgbKeyboard/mss_test.py
@@ -8,7 +8,7 @@ def test():
 
     mon = sct.monitors[1]
     left = mon["left"] + int(mon["width"]*0.01)
-    top = int(mon["height"]*0.7)
+    top = int(mon["height"]*0.0)
     right = mon["width"] - int(mon["width"]*0.01)
     lower = mon["height"]
 
@@ -28,10 +28,8 @@ def test():
         imS = cv2.resize(resized, (18*50, 6*50), interpolation = cv2.INTER_NEAREST) 
         cv2.imshow("test", imS)
 
-        if cv2.waitKey(1000//15) & 0xFF == ord("q"):
+        if cv2.waitKey(1000//60) & 0xFF == ord("q"):
             cv2.destroyAllWindows()
             break
-
-    return fps
 
 test()
