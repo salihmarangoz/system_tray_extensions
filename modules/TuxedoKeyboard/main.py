4517277f1e86490ce77b40523272273170044cb7
salihmarangoz <salih285@gmail.com>
2021-10-24 13:33:41 +0200

    added tuxedokeyboard persistent state

--- modules/TuxedoKeyboard/main.py
+++ modules/TuxedoKeyboard/main.py
@@ -7,7 +7,8 @@ from PyQt5.QtWidgets import *
 class TuxedoKeyboard:
     def __init__(self, node):
         self.node = node
-        self.gamma = (0.55, 0.48, 0.43)
+        self.gamma = (0.55, 0.48, 0.43) # needs to be calibrated
+        self.state = node.load_state()
 
         # init qt gui
         menu = node.get_tray_menu()
@@ -21,6 +22,16 @@ class TuxedoKeyboard:
         self.node.add_event_callback("lid_closed",   self.on_lid_closed)
         self.node.add_event_callback("on_ac",        self.on_ac)
         self.node.add_event_callback("on_battery",   self.on_battery)
+
+        if self.state is not None and "mode" in self.state:
+            if self.state["mode"] == "mono":
+                self.apply_lightbar_color(self.state["value"])
+            elif self.state["mode"] == "animation":
+                self.apply_lightbar_animation(self.state["value"])
+        else:
+            # default:
+            self.apply_lightbar_animation()
+
 
     def on_resume(self, event):
         pass
@@ -43,19 +54,13 @@ class TuxedoKeyboard:
     def init_gui(self, menu, app):
         self.lb = QMenu("LightBar")
         self.lb_ac0 = QAction("Turn Off");      self.lb_ac0.triggered.connect(lambda: self.apply_lightbar_color( (0.0,  0.0,  0.0)) );           self.lb.addAction(self.lb_ac0)
-        self.lb_ac1 = QAction("Animation");     self.lb_ac1.triggered.connect(lambda: self.apply_lightbar_animation(1) );                        self.lb.addAction(self.lb_ac1)
+        self.lb_ac1 = QAction("Animation");     self.lb_ac1.triggered.connect(lambda: self.apply_lightbar_animation() );                        self.lb.addAction(self.lb_ac1)
         self.lb_ac2 = QAction("White");         self.lb_ac2.triggered.connect(lambda: self.apply_lightbar_color( (1.0,  1.0,  1.0)) );           self.lb.addAction(self.lb_ac2)
         self.lb_ac3 = QAction("Red");           self.lb_ac3.triggered.connect(lambda: self.apply_lightbar_color( (1.0,    0,    0)) );           self.lb.addAction(self.lb_ac3)
         self.lb_ac4 = QAction("Green");         self.lb_ac4.triggered.connect(lambda: self.apply_lightbar_color( (  0,  1.0,    0)) );           self.lb.addAction(self.lb_ac4)
         self.lb_ac5 = QAction("Blue");          self.lb_ac5.triggered.connect(lambda: self.apply_lightbar_color( (  0,    0,  1.0)) );           self.lb.addAction(self.lb_ac5)
         self.lb_ac6 = QAction("Pick a color");  self.lb_ac6.triggered.connect(lambda: self.apply_lightbar_color( self.mono_color_picker()) );    self.lb.addAction(self.lb_ac6)
         menu.addMenu(self.lb)
-
-    def save_state(self, state):
-        pass # todo
-
-    def get_state(self):
-        return None # todo
 
     def color_to_voltage(self, color):
         return np.power(color, 1./np.array(self.gamma))
@@ -65,9 +70,7 @@ class TuxedoKeyboard:
         return (color[0]/255, color[1]/255, color[2]/255)
 
     def apply_lightbar_color(self, color):
-        self.apply_lightbar_voltage(self.color_to_voltage(color))
-
-    def apply_lightbar_voltage(self, voltage):
+        voltage = self.color_to_voltage(color)
         with open('/sys/class/leds/lightbar_rgb:1:status/brightness', 'w') as f:
             f.write(str(int(voltage[0]*36)))
         with open('/sys/class/leds/lightbar_rgb:2:status/brightness', 'w') as f:
@@ -75,6 +78,11 @@ class TuxedoKeyboard:
         with open('/sys/class/leds/lightbar_rgb:3:status/brightness', 'w') as f:
             f.write(str(int(voltage[2]*36)))
 
-    def apply_lightbar_animation(self, value):
+        self.state = {"mode": "mono", "value": color}
+        self.node.save_state(self.state)
+
+    def apply_lightbar_animation(self, value=1):
         with open('/sys/class/leds/lightbar_animation::status/brightness', 'w') as f:
             f.write(str(int(value)))
+        self.state = {"mode": "animation", "value": value}
+        self.node.save_state(self.state)
