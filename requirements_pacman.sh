8b79d3f94e97aed919640da1b69568d31277fbe7
Martynas M <invertisment+gh@pm.me>
2021-11-12 02:42:36 +0200

    Manjaro WIP (#39)
    * testing
    
    * testing
    
    * testing update again
    
    * testing
    
    * working update checks
    
    * removed test files
    
    * Manjaro WIP
    
    Co-authored-by: salihmarangoz <12991962+salihmarangoz@users.noreply.github.com>
    Co-authored-by: salihmarangoz <salih285@gmail.com>

---
+++ requirements_pacman.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Source: https://github.com/gfduszynski/cm-rgb/issues/45#issuecomment-920936950
+sudo pacman -S \
+  cairo \
+  gobject-introspection \
+  pkgconf \
+  gtk3 \
+  pkg-config
