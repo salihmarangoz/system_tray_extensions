1431afd7abebf85d874a02919f2c54d3c11df05d
salihmarangoz <salih285@gmail.com>
2021-11-13 19:40:08 +0100

    retry-13

--- requirements_pacman.sh
+++ requirements_pacman.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Source: https://github.com/gfduszynski/cm-rgb/issues/45#issuecomment-920936950
-sudo pacman -S \
+sudo pacman -S $PACMAN_EXTRA_ARGS \
   cairo \
   gobject-introspection \
   pkgconf \
