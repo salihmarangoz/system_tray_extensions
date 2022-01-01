7ab25ff2a333fc6f5fce96897dead2ed195b83ed
salihmarangoz <salih285@gmail.com>
2022-01-01 13:11:13 +0100

    testing check_import 4

--- .github/check_import.sh
+++ .github/check_import.sh
@@ -3,7 +3,7 @@ set -e
 SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 VENV_DIR="$SCRIPT_DIR"/../ste_env/bin/activate
 echo "SCRIPT_DIR=$SCRIPT_DIR"
-echo "VENV_DIR=$SCRIPT_DIR"
+echo "VENV_DIR=$VENV_DIR"
 
 source $VENV_DIR
 
